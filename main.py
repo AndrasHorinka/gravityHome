8831d940541f42092c0f5039562cf733ecab7532
AndrasHorinka <horinka.andras@gmail.com>
2019-06-27 10:21:34 +0200

    Samples into constants

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from services import fileReader
 
+SAMPLE_2 = "samples/sample_input_2_8.tsv"
+SAMPLE_3 = "samples/sample_input_3_1000.tsv"
+SAMPLE_4 = "samples/sample_input_4_4.tsv"
+SAMPLE_10 = "samples/sample_input_10_100.tsv"
+SAMPLE_100 = "samples/sample_input_100_100.tsv"
+
+OUTPUT_2 = "samples/sample_output_2_8.txt"
+OUTPUT_3 = "samples/sample_output_3_100.txt"
+OUTPUT_4 = "samples/sample_output_4_4.txt"
+OUTPUT_10 = "samples/sample_output_10_100.txt"
+OUTPUT_100 = "samples/sample_output_100_100.txt"
+
 
 def main():
-   print("main started")
+   sample2 = fileReader.get_table_from_file(SAMPLE_2)
+   output2 = fileReader.get_table_from_file(OUTPUT_2)
+   print(sample2[0][0])
+   print(output2)
 
 
 if __name__ == '__main__':
