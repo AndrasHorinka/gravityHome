be429b2ad178a89db39a2a1a4c15eb767bd6a259
AndrasHorinka <horinka.andras@gmail.com>
2019-06-27 10:20:05 +0200

    fileReader Init

--- services/fileReader.py
+++ services/fileReader.py
@@ -11,6 +11,6 @@ def get_table_from_file(file_name):
     """
     with open(file_name, "r") as file:
         lines = file.readlines()
-    table = [element.replace("\n", "").split(";") for element in lines]
+    table = [element.replace("\n", "").split("\t") for element in lines]
 
     return table
