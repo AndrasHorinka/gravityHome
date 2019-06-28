224920d8813a637c5d63080e4f7d79892efe607b
AndrasHorinka <horinka.andras@gmail.com>
2019-06-28 16:15:01 +0200

    functional test cases

--- test.py
+++ test.py
@@ -13,14 +13,14 @@ class TestFileReader(unittest.TestCase):
     def test_fileReader_sample_2(self):
         data_file = fileReader.get_table_from_file(self.SAMPLE_2)
         expected_data_file = [
-            [-262972, 508697],
-            [-311943.65362731507, 370239.3559213022],
-            [742431, 772652],
-            [-346046, 696615.3537438104],
-            [194172, 103527],
-            [726621.8167057682, 813087.8844925504],
-            [167923, 312455.0459619701],
-            [499664.42762545496, 72395.09685360803]
+            [str(-262972), str(508697)],
+            [str(-311943.65362731507), str(370239.3559213022)],
+            [str(742431), str(-772652)],
+            [str(-346046), str(696615.3537438104)],
+            [str(194172), str(103527)],
+            [str(726621.8167057682), str(-813087.8844925504)],
+            [str(167923), str(-312455.0459619701)],
+            [str(499664.42762545496), str(72395.09685360803)]
         ]
 
         self.assertEqual(data_file, expected_data_file, "FileReader Sample 2 is NOT OK!")
@@ -28,10 +28,10 @@ class TestFileReader(unittest.TestCase):
     def test_fileReader_sample_4(self):
         data_file = fileReader.get_table_from_file(self.SAMPLE_4)
         expected_data_file = [
-            [653725, -422117.9078937047, -483855, -579967],
-            [-543446.8852285817, -319599.12096280814, 130119, 42410.4020668423],
-            [-800188, 58364, 586736, -409415],
-            [-607784.1676060366, 400299, -925526.2019860733, -779617]
+            [str(653725), str(-422117.9078937047), str(-483855), str(-579967)],
+            [str(-543446.8852285817), str(-319599.12096280814), str(130119), str(42410.4020668423)],
+            [str(-800188), str(58364), str(586736), str(-409415)],
+            [str(-607784.1676060366), str(400299), str(-925526.2019860733), str(-779617)]
         ]
 
         self.assertEqual(data_file, expected_data_file, "FileReader Sample 4 is NOT OK!")
@@ -60,50 +60,138 @@ class TestMathematicalImplementationGetSquareOfCoordinates(unittest.TestCase):
     def test_get_squares_of_coordinates_2d_m1_p1_p1_m1(self):
         node_a = [-1, 1]
         node_b = [1, -1]
-        result = 8
+        result = int()
         for n in mi.get_squares_of_coordinates(node_a, node_b):
             result += n
 
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 8)
 
     def test_get_squares_of_coordinates_2d_m10_m20_p10_m20(self):
         node_a = [-10, -20]
         node_b = [10, 20]
-        result = 2000
+        result = int()
         for n in mi.get_squares_of_coordinates(node_a, node_b):
             result += n
 
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 2000)
 
     def test_get_squares_of_coordinates_2d_m10_m20_p0_m0(self):
         node_a = [-10, -20]
         node_b = [0, 0]
-        result = 500
+        result = int()
         for n in mi.get_squares_of_coordinates(node_a, node_b):
             result += n
 
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 500)
 
     def test_get_squares_of_coordinates_3d_m10_m20_p0_m0(self):
         node_a = [-10, -20]
         node_b = [0, 0]
-        result = 500
+        result = int()
         for n in mi.get_squares_of_coordinates(node_a, node_b):
             result += n
 
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 500)
 
-    def test_get_squares_of_coordinates_null_node(self):
+    def test_get_squares_of_coordinates_incorrect_type(self):
+        # TODO: check why typeerror is not raised
+        node_a = 2
+        node_b = [0, 0]
+        with self.assertRaises(TypeError):
+            mi.get_squares_of_coordinates(node_a, node_b)
+
+    def test_get_squares_of_coordinates_zeroLengthList(self):
+        # TODO: check why assertion error is not thrown
         node_a = []
         node_b = [0, 0]
-        self.assertRaises
-        self.assertEqual(result, 0)
+        with self.assertRaises(AssertionError):
+            mi.get_squares_of_coordinates(node_a, node_b)
 
-# class TestMathematicalImplementationGetSquareOfCoordinates(unittest.TestCase):
 
-# TODO: create assertion tests if the two nodes doesn't have the same length
-# TODO: create assertion test if the coordinates are null of a node (node_a[])
-# TODO: create TypeAssertion test if the coordinates are not int/float - or can't be converted to it
+class TestMathematicalImplementationGetDistanceSquare(unittest.TestCase):
+
+    def test_get_distance_square_incorrectNodeAType(self):
+        node_a = 2
+        node_b = [0, 0]
+        with self.assertRaises(TypeError):
+            mi.get_distance_square(node_a, node_b)
+
+    def test_get_distance_square_incorrectNodeBType(self):
+        node_a = [2, 2]
+        node_b = 2
+        with self.assertRaises(TypeError):
+            mi.get_distance_square(node_a, node_b)
+
+    def test_get_distance_square_zeroLengthList(self):
+        node_a = []
+        node_b = [0, 0]
+        with self.assertRaises(AssertionError):
+            mi.get_distance_square(node_a, node_b)
+
+    def test_get_distance_square_differentLengthLists(self):
+        node_a = [1, 1]
+        node_b = [2, 2, 2]
+        with self.assertRaises(AssertionError):
+            mi.get_distance_square(node_a, node_b)
+
+
+class TestMathematicalImplementationGetTheClosestNodeFromOrigin(unittest.TestCase):
+
+    def test_get_the_closest_node_from_origin_incorrectOriginType(self):
+        node_a = 2
+        nodes = [[0, 0], [1, 1]]
+        with self.assertRaises(TypeError):
+            mi.get_the_closest_node_from_origin(node_a, nodes)
+
+    def test_get_the_closest_node_from_origin_incorrectNodesType(self):
+        node_a = [1, 1]
+        nodes = [2, 3]
+        with self.assertRaises(TypeError):
+            mi.get_the_closest_node_from_origin(node_a, nodes)
+
+    def test_get_the_closest_node_from_origin_noOriginCoordinates(self):
+        node_a = []
+        nodes = [[0, 0], [1, 1]]
+        with self.assertRaises(AssertionError):
+            mi.get_the_closest_node_from_origin(node_a, nodes)
+
+    def test_get_the_closest_node_from_origin_noDestinationCoordinates(self):
+        node_a = [1, 1]
+        nodes = [[0], [1, 1]]
+        with self.assertRaises(TypeError):
+            mi.get_the_closest_node_from_origin(node_a, nodes)
+
+
+class TestMathematicalImplementationGetTheShortestDistanceForEachNodes(unittest.TestCase):
+
+    def test_get_the_shortest_distance_for_each_node_nodesWithNoElements(self):
+        nodes = []
+        with self.assertRaises(AssertionError):
+            mi.get_the_shortest_distance_for_each_node(nodes)
+
+    def test_get_the_shortest_distance_for_each_node_nodesWithOneElementOnly(self):
+        nodes = [1, 1]
+        with self.assertRaises(AssertionError):
+            mi.get_the_shortest_distance_for_each_node(nodes)
+
+    def test_get_the_shortest_distance_for_each_node_nodesWithDifferentDimensions(self):
+        nodes = [[1, 1], [0, 1, 2]]
+        with self.assertRaises(AssertionError):
+            mi.get_the_shortest_distance_for_each_node(nodes)
+
+
+class TestMathematicalImplementationGetTheindexOfTheClosestNode(unittest.TestCase):
+
+    def test_get_the_index_of_the_closest_node_nodesWithNoElements(self):
+        nodes = []
+        with self.assertRaises(AssertionError):
+            mi.get_the_index_of_the_closest_node(nodes)
+
+    def test_get_the_index_of_the_closest_node_nodesWithOneElementOnly(self):
+        nodes = [1, 1]
+        with self.assertRaises(AssertionError):
+            mi.get_the_index_of_the_closest_node(nodes)
+
 
 def main():
     unittest.main()
