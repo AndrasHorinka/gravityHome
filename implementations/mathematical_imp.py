52a3e0d35aa6f94be3e5a15519f68e6a902e6d41
AndrasHorinka <horinka.andras@gmail.com>
2019-06-30 14:33:45 +0200

    restructured math implementation, but performance optimized

--- implementations/mathematical_imp.py
+++ implementations/mathematical_imp.py
@@ -3,26 +3,21 @@
 # As we can assume that if norm(A) > norm (B) than sqrt(A) > sqrt(B).
 
 
-def get_the_index_of_the_closest_nodes(nodes):
+def get_squares_of_coordinates(node_a, node_b):
     """
-    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
-    :return: tuple(index of Node_a, index of Node_b) --> indexes of the two closest nodes in the list of Nodes (N)
+    :param node_a: Node(A) with coordinates. N = [coord1, coord2, ... coordn)
+    :param node_b: Node(B) with coordinates. N = [coord1, coord2, ... coordn)
+    :return: An int referring to the square of the difference of coordinates at the same dimension. (A[i] - B[i]) ** 2
     """
     try:
-        assert len(nodes) > 2, "Only one node found"
+        assert len(node_a) != 0, "Node A has no coordinates."
+        assert len(node_b) != 0, "Node B has no coordinates."
+        assert len(node_a) == len(node_b), "Nodes have different dimensions"
 
-        distances = get_the_shortest_distance_for_each_node(nodes)
-        min_distance = float("inf")
-        node_a_index = int()
-        node_b_index = int()
-
-        for key, value in distances.items():
-            if value[1] < min_distance:
-                min_distance = value[1]
-                node_a_index = key + 1
-                node_b_index = value[0] + key + 2
-
-        return node_a_index, node_b_index
+        i = 0
+        while i < len(node_a):
+            yield (float(node_a[i]) - float(node_b[i])) ** 2
+            i += 1
 
     except AssertionError as e:
         raise e from None
@@ -32,27 +27,25 @@ def get_the_index_of_the_closest_nodes(nodes):
         raise e from None
 
 
-def get_the_shortest_distance_for_each_node(nodes):
+def get_distance_square(
+        node_a,
+        node_b,
+        _get_squares_of_coordinates=get_squares_of_coordinates):
     """
-    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
-    :return: a dictionary where index of N(i) is the key and value holds a tuple of (index N(other), distance from N(other))
+    :param node_a: Node(A) with coordinates. N = [coord1, coord2, ... coordn)
+    :param node_b: Node(B) with coordinates. N = [coord1, coord2, ... coordn)
+    :return: An int referring to the square of distance of Node(A) - Node(B).
     """
     try:
-        assert len(nodes) > 2, "Only one node found"
+        assert len(node_a) != 0, "Node A has no coordinates."
+        assert len(node_b) != 0, "Node B has no coordinates."
+        assert len(node_a) == len(node_b), "Nodes have different dimensions"
 
-        distances = dict()
+        dist = 0
+        for n in _get_squares_of_coordinates(node_a, node_b):
+            dist += n
 
-        for i, node in enumerate(nodes):
-            if i == len(nodes) - 1:
-                break
-
-            assert len(node) == len(nodes[i + 1]), str(node) + " & " + str(
-                nodes[i + 1]) + " don't have the same dimensions"
-
-            distances[i] = get_the_closest_node_from_origin(
-                node, nodes[i + 1:])
-
-        return distances
+        return dist
 
     except AssertionError as e:
         raise e from None
@@ -62,7 +55,8 @@ def get_the_shortest_distance_for_each_node(nodes):
         raise e from None
 
 
-def get_the_closest_node_from_origin(origin, nodes):
+def get_the_closest_node_from_origin(
+        origin, nodes, _get_distance_square=get_distance_square):
     """
     :param origin: Source Node to compare N(others) to. N = [coord1, coord2, ... coordn)
     :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
@@ -80,7 +74,7 @@ def get_the_closest_node_from_origin(origin, nodes):
             assert len(origin) == len(nodes[i]), str(
                 origin) + " & " + str(nodes[i]) + " don't have the same dimensions"
 
-            n_distance = get_distance_square(origin, nodes[i])
+            n_distance = _get_distance_square(origin, nodes[i])
 
             if max_distance > n_distance:
                 max_distance = n_distance
@@ -96,22 +90,28 @@ def get_the_closest_node_from_origin(origin, nodes):
         raise e from None
 
 
-def get_distance_square(node_a, node_b):
+def get_the_shortest_distance_for_each_node(
+        nodes, _get_the_closest_node_from_origin=get_the_closest_node_from_origin):
     """
-    :param node_a: Node(A) with coordinates. N = [coord1, coord2, ... coordn)
-    :param node_b: Node(B) with coordinates. N = [coord1, coord2, ... coordn)
-    :return: An int referring to the square of distance of Node(A) - Node(B).
+    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
+    :return: a dictionary where index of N(i) is the key and value holds a tuple of (index N(other), distance from N(other))
     """
     try:
-        assert len(node_a) != 0, "Node A has no coordinates."
-        assert len(node_b) != 0, "Node B has no coordinates."
-        assert len(node_a) == len(node_b), "Nodes have different dimensions"
+        assert len(nodes) > 2, "Only one node found"
 
-        dist = 0
-        for n in get_squares_of_coordinates(node_a, node_b):
-            dist += n
+        distances = dict()
 
-        return dist
+        for i, node in enumerate(nodes):
+            if i == len(nodes) - 1:
+                break
+
+            assert len(node) == len(nodes[i + 1]), str(node) + " & " + str(
+                nodes[i + 1]) + " don't have the same dimensions"
+
+            distances[i] = _get_the_closest_node_from_origin(
+                node, nodes[i + 1:])
+
+        return distances
 
     except AssertionError as e:
         raise e from None
@@ -121,21 +121,28 @@ def get_distance_square(node_a, node_b):
         raise e from None
 
 
-def get_squares_of_coordinates(node_a, node_b):
+def get_the_index_of_the_closest_nodes(
+        nodes,
+        _get_the_shortest_distance_for_each_node=get_the_shortest_distance_for_each_node):
     """
-    :param node_a: Node(A) with coordinates. N = [coord1, coord2, ... coordn)
-    :param node_b: Node(B) with coordinates. N = [coord1, coord2, ... coordn)
-    :return: An int referring to the square of the difference of coordinates at the same dimension. (A[i] - B[i]) ** 2
+    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
+    :return: tuple(index of Node_a, index of Node_b) --> indexes of the two closest nodes in the list of Nodes (N)
     """
     try:
-        assert len(node_a) != 0, "Node A has no coordinates."
-        assert len(node_b) != 0, "Node B has no coordinates."
-        assert len(node_a) == len(node_b), "Nodes have different dimensions"
+        assert len(nodes) > 2, "Only one node found"
 
-        i = 0
-        while i < len(node_a):
-            yield (float(node_a[i]) - float(node_b[i])) ** 2
-            i += 1
+        distances = _get_the_shortest_distance_for_each_node(nodes)
+        min_distance = float("inf")
+        node_a_index = int()
+        node_b_index = int()
+
+        for key, value in distances.items():
+            if value[1] < min_distance:
+                min_distance = value[1]
+                node_a_index = key + 1
+                node_b_index = value[0] + key + 2
+
+        return node_a_index, node_b_index
 
     except AssertionError as e:
         raise e from None
