0a45c54e45bd12dc9db2597075f4f12c9ef13244
AndrasHorinka <horinka.andras@gmail.com>
2019-06-30 13:54:05 +0200

    docstrings for SciPy

--- implementations/sciPy_imp.py
+++ implementations/sciPy_imp.py
@@ -2,6 +2,10 @@ from scipy.spatial import distance
 
 
 def calculate_distance_of_each_nodes(nodes):
+    """
+    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
+    :return: an NxM matrix where N stands for the Nodes and M stands for the distance of N(i) to N(each other)
+    """
     try:
         assert all([True for node in nodes if len(node) == len(nodes[0])]), "Not all nodes share the same dimension"
         assert all([[True for dist in node if isinstance(float(dist), float)] for node in
@@ -18,6 +22,10 @@ def calculate_distance_of_each_nodes(nodes):
 
 
 def replace_zeros(distances):
+    """
+    :param distances: an NxM matrix where N stands for the Nodes and M stands for the distance of N(i) to N(each other)
+    :return: an NxM matrix where N stands for the Nodes and M stands for the distance of N(i) to N(each other); where all zeroes are replaced by max+2 value.
+    """
     try:
         assert len(distances) > 2, "There aren't enough nodes to compare"
 
@@ -33,6 +41,10 @@ def replace_zeros(distances):
 
 
 def get_the_minimum_distance_of_nodes(distances):
+    """
+    :param distances: an NxM matrix where N stands for the Nodes and M stands for the distance of N(i) to N(each other)
+    :return: the smallest value from NxM matrix
+    """
     try:
         return min([min(dist_from_node) for dist_from_node in distances])
 
@@ -43,6 +55,10 @@ def get_the_minimum_distance_of_nodes(distances):
 
 
 def get_the_index_of_the_closest_nodes(nodes):
+    """
+    :param nodes: an NxM matrix where N stands for Nodes and M stands for the coordinates of the Nodes
+    :return: tuple(index of Node_a, index of Node_b) --> indexes of the two closest nodes in the list of Nodes (N)
+    """
     try:
         assert all([True for node in nodes if len(node) == len(nodes[0])]), "Not all nodes share the same dimension"
         assert all([[True for dist in node if isinstance(float(dist), float)] for node in
